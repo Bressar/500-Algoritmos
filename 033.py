4d8b6b94ab00ff3b347f32da4807872c6c409f0b
bressar <dougbressar@gmail.com>
2024-09-10 10:29:37 +0100

    10_09

--- 033.py
+++ 033.py
@@ -46,15 +46,53 @@ def inserir_elemento_na_lista(lista):
         else:
             print("Inválido! escolha novamente!")       
     print(lista)
-    
+
 # inserir_elemento_na_lista(lista1) # debug do código
 
 
-# Pilha (Stack) FILO - first in, last out
-pilha1 = []
 
-def gerenciar_pilha(fila):
-    print("O que deseja fazer ?")  
+# Pilha (Stack) FILO - first in, last out
+#  onde o último item adicionado é o primeiro a ser removido
+pilha2 = []
+
+def gerenciar_pilha(pilha):
+    print("O que deseja fazer?")  
+    while True:
+        resposta = str(input("[A]dicionar - [R]emover - [S]air ")).strip().upper()
+        if resposta == "A":
+            while True:
+                try:
+                    numero = int(input('Digite um número: '))
+                    pilha.append(numero)
+                    print(f"Pilha atual: {pilha}")
+                    break
+                except ValueError:
+                    print('Valor inválido, tente novamente!') 
+                                     
+        elif resposta == "R":
+            if pilha:
+                removido = pilha.pop()
+                print(f"{removido} foi removido do topo da pilha com sucesso!")
+                print(f"Pilha atual: {pilha}")
+            else:
+                print("A pilha está vazia, não há o que remover.")
+                         
+        elif resposta == "S":
+            print(f"Pilha final: {pilha}")
+            break
+        else:
+            print("Opção inválida! Escolha novamente.")  
+
+#gerenciar_pilha(pilha2) # debug do código
+
+
+
+# Fila (Queue) FIFO - first in, first out
+# #  onde o primeiro item adicionado é o primeiro a ser removido
+fila = []
+
+def gerenciar_fila(fila):
+    print("O que deseja fazer?")  
     while True:
         resposta = str(input("[A]dicionar - [R]emover - [S]air ")).strip().upper()
         if resposta == "A":
@@ -62,68 +100,99 @@ def gerenciar_pilha(fila):
                 try:
                     numero = int(input('Digite um número: '))
                     fila.append(numero)
+                    print(f"Fila atual: {fila}")
                     break
                 except ValueError:
-                    print('Valor inválido, tente novamente!') 
+                    print('Valor inválido, tente novamente!')
                                      
         elif resposta == "R":
-            try:
-                numero_out = int(input('Digite o número a ser removido do topo da pilha: '))
-                if numero_out == fila[-1]:
-                    fila.pop()
-                    print(f"{numero_out} foi removido do topo da pilha com sucesso!")
-                    print(f"{fila}")
-                else:
-                    print(f"{numero_out} não é o último item no topo da pilha!")
-            except ValueError:
-                print("Insira um valor válido!")
+            if len(fila) == 0:
+                print("A fila está vazia, não há o que remover!")
+            else:
+                removido = fila.pop(0)
+                print(f"{removido} foi removido do início da fila com sucesso!")
+                print(f"Fila atual: {fila}")
                          
         elif resposta == "S":
-            print(fila)
+            print(f"Fila final: {fila}")
             break
         else:
-            print("Inválido! escolha novamente!")  
-     
-             
-#gerenciar_pilha(pilha1)
+            print("Opção inválida! Escolha novamente.")
+
+gerenciar_fila(fila)
+
+
+
+# Pilha (Stack) FILO - first in, last out
+# versão com opção de escolher um número, o tradicional seria sempre remover o último item
+# pilha1 = []
+
+# def gerenciar_pilha(pilha):
+#     print("O que deseja fazer ?")  
+#     while True:
+#         resposta = str(input("[A]dicionar - [R]emover - [S]air ")).strip().upper()
+#         if resposta == "A":
+#             while True:
+#                 try:
+#                     numero = int(input('Digite um número: '))
+#                     pilha.append(numero)
+#                     break
+#                 except ValueError:
+#                     print('Valor inválido, tente novamente!') 
+#         elif resposta == "R":
+#             try:
+#                 numero_out = int(input('Digite o número a ser removido do topo da pilha: '))
+#                 if numero_out == pilha[-1]:
+#                     pilha.pop()
+#                     print(f"{numero_out} foi removido do topo da pilha com sucesso!")
+#                     print(f"{pilha}")
+#                 else:
+#                     print(f"{numero_out} não é o último item no topo da pilha!")
+#             except ValueError:
+#                 print("Insira um valor válido!")                        
+#         elif resposta == "S":
+#             print(pilha)
+#             break
+#         else:
+#             print("Inválido! escolha novamente!")  
+                 
+# gerenciar_pilha(pilha1)
 
 
 # Fila - firts in, first out
-fila1 = []
+# com a opção de escolher um número a ser removido
+# fila1 = []
 
-def gerenciar_fila(fila):
-    print("O que deseja fazer ?")  
-    while True:
-        resposta = str(input("[A]dicionar - [R]emover - [S]air ")).strip().upper()
-        if resposta == "A":
-            while True:
-                try:
-                    numero = int(input('Digite um número: '))
-                    fila.append(numero)
-                    break
-                except ValueError:
-                    print('Valor inválido, tente novamente!') 
-                                     
-        elif resposta == "R":
-            if len(fila) == 0:  # Verifica se a fila está vazia
-                print("A fila está vazia, não há o que remover!")
-            else:
-                try:
-                    numero_out = int(input('Digite o número a ser removido do início da fila: '))
-                    if numero_out == fila[0]:
-                        fila.pop(0)
-                        print(f"{numero_out} foi removido do início da fila com sucesso!")
-                        print(f"{fila}")
-                    else:
-                        print(f"{numero_out} não é o primeiro item da fila!")
-                except ValueError:
-                    print("Insira um valor válido!")
-                         
-        elif resposta == "S":
-            print(fila)
-            break
-        else:
-            print("Inválido! escolha novamente!")  
-     
-             
-gerenciar_fila(fila1)
+# def gerenciar_fila(fila):
+#     print("O que deseja fazer ?")  
+#     while True:
+#         resposta = str(input("[A]dicionar - [R]emover - [S]air ")).strip().upper()
+#         if resposta == "A":
+#             while True:
+#                 try:
+#                     numero = int(input('Digite um número: '))
+#                     fila.append(numero)
+#                     break
+#                 except ValueError:
+#                     print('Valor inválido, tente novamente!') 
+#         elif resposta == "R":
+#             if len(fila) == 0:  # Verifica se a fila está vazia
+#                 print("A fila está vazia, não há o que remover!")
+#             else:
+#                 try:
+#                     numero_out = int(input('Digite o número a ser removido do início da fila: '))
+#                     if numero_out == fila[0]:
+#                         fila.pop(0)
+#                         print(f"{numero_out} foi removido do início da fila com sucesso!")
+#                         print(f"{fila}")
+#                     else:
+#                         print(f"{numero_out} não é o primeiro item da fila!")
+#                 except ValueError:
+#                     print("Insira um valor válido!")
+#         elif resposta == "S":
+#             print(fila)
+#             break
+#         else:
+#             print("Inválido! escolha novamente!")  
+              
+# # gerenciar_fila(fila1)
