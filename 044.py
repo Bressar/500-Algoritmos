0630dab4061e693e7a8c16c6ee658c5ee30a4efd
bressar <dougbressar@gmail.com>
2024-09-18 22:12:41 +0100

    18_09

--- 044.py
+++ 044.py
@@ -1,1 +1,21 @@
 # Algoritmo 44
+# Fatorial
+
+# Usando while
+
+numero = int(input("Insira um número para saber o seu fatorial: "))
+fatorial = 1 #começa em 1 para não ser multiplicado por zero
+i = 2
+while i <= numero: # vai parar de multiplicar quando chegar no valor do numero
+    fatorial = fatorial * i # vai multiplicando a cada loop com um valor maior de i até i chegar no valor do numero
+    i = i + 1 # vai começar em e adicionando de 1 em 1 até chegar no numero
+
+print(f"O valor de {numero}! é = {fatorial}")
+
+# Usando for
+fatorial2 = 1
+for i in range (numero, 0, -1):
+    fatorial2 *= i
+    i * (i - 1)
+    print (i, " X ", i -1, )# for debug
+print(fatorial2)
--- rascunho.py
+++ rascunho.py
@@ -1,20 +1,27 @@
-def dis(price, discount):
-    price_discount = price - ((price * discount) / 100)
-    return round(price_discount, 2)
+num = int(input())
+
+for i in range(0, num + 1):
+    if (i % 2 != 0):
+    	print (i, end='\n')
+    
+
+# def dis(price, discount):
+#     price_discount = price - ((price * discount) / 100)
+#     return round(price_discount, 2)
 		
 		
-print(dis(1500, 50))
-print(dis(89, 20))
-print(dis(100, 75))
+# print(dis(1500, 50))
+# print(dis(89, 20))
+# print(dis(100, 75))
 
-print('--' * 25)
+# print('--' * 25)
 
-def dis(price, discount):
-	discount = discount / 100
-	newPrice = price - (price * discount)
-	return round(newPrice,2)
+# def dis(price, discount):
+# 	discount = discount / 100
+# 	newPrice = price - (price * discount)
+# 	return round(newPrice,2)
 
 
-print(dis(1500, 50))
-print(dis(89, 20))
-print(dis(100, 75))
+# print(dis(1500, 50))
+# print(dis(89, 20))
+# print(dis(100, 75))
