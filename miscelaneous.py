8b7435630d740aecf97233067793cd336b3f8768
bressar <dougbressar@gmail.com>
2025-02-25 11:10:50 +0000

    misc_01

--- miscelaneous.py
+++ miscelaneous.py
@@ -1,78 +1,140 @@
 # Testes de código, comandos e funções básicas do python
 
 
+# 008 *Args e ** Kwargs
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 007 Linhas mútiplas
+# menu= """
+# ========== MENU =========
+
+#  1 - Sacar
+ 
+#  2 - Depositar
+ 
+#  0 - Sair
+
+# =========================
+
+#         Obrigado por usar o nosso sistema!!
+
+# """
+# print(menu)
+
+
+# 006 strings
+""" nome1 = "Guilherme Arthur de Carvalho"
+
+print(nome1[0]) # G
+print(nome1[:9]) # Guilherme
+print(nome1[10:]) # Arthur de Carvalho
+print(nome1[10:16]) # Arthur
+print(nome1[10:16:2]) # Atu
+print(nome1[:]) # Guilherme Arthur de Carvalho
+print(nome1[::-1]) # ohlavraC ed ruhtrA emrehliuG
+ 
+nome ='Douglas Funny'
+print(nome[0])
+print(nome[:9])
+print(nome[:10])
+print(nome[5:10])
+
+print(nome[4:12:2])
+
+palavra = 'text'
+
+print(palavra.center(10, '#')) 
+# ###text###
+
+print('.'.join(palavra))
+# t.e.x.t """ """
 
 # 005
-while True:
-    numero = int(input('Informe um número: '))   
-    if numero == 10:
-        break 
+# while True:
+#     numero = int(input('Informe um número: '))   
+#     if numero == 10:
+#         break 
     
-    if numero % 2 == 0:
-        continue
+#     if numero % 2 == 0:
+#         continue
        
-    print(numero)
+#     print(numero)
 
-print('\n')
+# print('\n')
 
 
-for numero in range(100):    
-    if numero == 10:
-        break # continue 'pula'  
-    print(numero, end =' ')
+# for numero in range(100):    
+#     if numero == 10:
+#         break # continue 'pula'  
+#     print(numero, end =' ')
     
-print('\n')
+# print('\n')
     
-for numero in range(100):
-    if numero  % 2 == 0: # pula os pares
-        continue
-    print(numero, end =' ')
+# for numero in range(100):
+#     if numero  % 2 == 0: # pula os pares
+#         continue
+#     print(numero, end =' ')
 
 
 
 # 004
-""" opcao = -1
+# opcao = -1
 
-while opcao != 0:
-    opcao = int(input('[1] Sacar\n[2] Extrato\n[0] Sair\n:'))
+# while opcao != 0:
+#     opcao = int(input('[1] Sacar\n[2] Extrato\n[0] Sair\n:'))
     
-    if opcao == 1:
-        print('sacando...')
-    elif opcao == 2:
-        print('Exibindo o extrato...')
-else:
-    print('Obrigado por usar o nosso sistema') """
+#     if opcao == 1:
+#         print('sacando...')
+#     elif opcao == 2:
+#         print('Exibindo o extrato...')
+# else:
+#     print('Obrigado por usar o nosso sistema') """
         
 
-# 003
-""" for numero in range(0, 11):
-    print(numero, end=' ')
+# # 003
+# """ for numero in range(0, 11):
+#     print(numero, end=' ')
 
-print('\n')
+# print('\n')
     
-for numero in range (0, 51, 5):
-    print(numero, end= ' ') """
+# for numero in range (0, 51, 5):
+#     print(numero, end= ' ') """
 
 
-# 002
-# range (stop) -> range object
-# range(start, stop[,step]) -> range object
+# # 002
+# # range (stop) -> range object
+# # range(start, stop[,step]) -> range object
 
-""" print(range(4)) # range(0, 4)
+# """ print(range(4)) # range(0, 4)
 
-print(list(range(4))) # [0, 1, 2, 3]  
+# print(list(range(4))) # [0, 1, 2, 3]  
 
- """
+#  """
 
-# 001
-""" texto = input('Escreva um texto:\n')
+# # 001
+# """ texto = input('Escreva um texto:\n')
 
-vogais = 'AEIOU'
+# vogais = 'AEIOU'
 
-for letra in texto:
-    if letra.upper() in vogais: # remove as consoantes
-        print(letra, end='')
-else:       
-    print()
-    print('Executa no final do laço') """
+# for letra in texto:
+#     if letra.upper() in vogais: # remove as consoantes
+#         print(letra, end='')
+# else:       
+#     print()
+#     print('Executa no final do laço') """
