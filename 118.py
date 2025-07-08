87a18339ee5b064093f0a0dc5c1d65f24085a6c3
bressar <dougbressar@gmail.com>
2025-07-08 16:38:09 +0100

    Update 118.py

--- 118.py
+++ 118.py
@@ -9,17 +9,35 @@
 """
 
 # Exemplo
-
+""" meu_gerador é uma função que recebe uma lista de números.
+Para cada número na lista, ela multiplica o número por 2 e entrega (yield) esse valor.
+ """
 def meu_gerador(numeros: list[int]):
     for numero in numeros:
         yield numero * 2
-
+        
+""" yield é uma palavra reservada do Python que transforma uma função comum em um gerador (generator).
+Comparando com return:
+return encerra a função e entrega um valor de uma vez só.
+yield pausa a função, entrega um valor e mantém o estado da função para continuar de onde parou.
+Assim, com yield, a função pode continuar de onde parou na próxima iteração.        
+ """
 
 for i in meu_gerador(numeros=[1,2,3]):
     print(i)
 
+""" chama a função meu_gerador com [1, 2, 3].
+Como a função usa yield, ela retorna um objeto gerador, que é iterado pelo for.
+A cada iteração, o gerador:
+Executa até o próximo yield.
+Retorna numero * 2.
+ """
 
+""" Resumo:
+yield transforma a função em um gerador.
 
+O gerador produz valores sob demanda, um por vez, de forma eficiente em memória.
+ """
 
 
 
